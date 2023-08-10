7f92edaaac3a6f06ab941c4e5ca325e2c04a42be
EnderWarik <2020102437@pnu.edu.ru>
2023-08-10 19:22:14 +1000

    reorganize User

--- src/main/kotlin/ru/itcompany/models/User.kt
+++ src/main/kotlin/ru/itcompany/models/User.kt
@@ -8,6 +8,7 @@ import org.ktorm.schema.enum
 import org.ktorm.schema.long
 import org.ktorm.schema.varchar
 import ru.itcompany.models.enum.UserRoleEnum
+import ru.itcompany.service.user.argument.RegisterUserArgument
 
 
 interface  User : Entity<User> {
@@ -23,18 +24,6 @@ interface  User : Entity<User> {
     var phoneNumber: String?
     var inn: String?
     var organizationName: String?
+
 }
-val Database.users get() = this.sequenceOf(Users)
-object Users : Table<User>("users") {
-    val id = long("id").primaryKey().bindTo { it.id }
-    val email = varchar("email").bindTo { it.email }
-    val password = varchar("password").bindTo { it.password }
-    val role = enum<UserRoleEnum>("role").bindTo { it.role }
-    val firstName = varchar("first_name").bindTo { it.firstName }
-    val lastName = varchar("last_name").bindTo { it.lastName }
-    val thirdName = varchar("third_name").bindTo { it.thirdName }
-    val address = varchar("address").bindTo { it.address }
-    val phoneNumber = varchar("phone_number").bindTo { it.phoneNumber }
-    val inn = varchar("inn").bindTo { it.inn }
-    val organizationName = varchar("organization_name").bindTo { it.organizationName }
-}
+
---
+++ src/main/kotlin/ru/itcompany/models/dao/UserDao.kt
@@ -0,0 +1,26 @@
+package ru.itcompany.models.dao
+
+import org.ktorm.database.Database
+import org.ktorm.entity.Entity
+import org.ktorm.entity.sequenceOf
+import org.ktorm.schema.Table
+import org.ktorm.schema.enum
+import org.ktorm.schema.long
+import org.ktorm.schema.varchar
+import ru.itcompany.models.User
+import ru.itcompany.models.enum.UserRoleEnum
+
+val Database.users get() = this.sequenceOf(Users)
+object Users : Table<User>("users") {
+    val id = long("id").primaryKey().bindTo { it.id }
+    val email = varchar("email").bindTo { it.email }
+    val password = varchar("password").bindTo { it.password }
+    val role = enum<UserRoleEnum>("role").bindTo { it.role }
+    val firstName = varchar("first_name").bindTo { it.firstName }
+    val lastName = varchar("last_name").bindTo { it.lastName }
+    val thirdName = varchar("third_name").bindTo { it.thirdName }
+    val address = varchar("address").bindTo { it.address }
+    val phoneNumber = varchar("phone_number").bindTo { it.phoneNumber }
+    val inn = varchar("inn").bindTo { it.inn }
+    val organizationName = varchar("organization_name").bindTo { it.organizationName }
+}
